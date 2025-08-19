6b9c9238970e5508f14694e8340f499936295a03
Machado143 <machadogm14@gmail.com>
2025-08-19 10:48:31 -0300

    2

--- Farmacia/models.py
+++ Farmacia/models.py
@@ -7,7 +7,29 @@ class Medicamento(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     estoque = models.IntegerField()
 
+
+
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     telefone = models.CharField(max_length=15)
+
+    medicamento = models.ManyToManyField(
+        to=Medicamento,
+        related_name='clientes',
+        through='ClienteMedicamento',
+        through_fields=('cliente', 'medicamento'),
+    )
+
+
+
+class ClienteMedicamento(models.Model):
+    cliente = models.ForeignKey(
+        to=Cliente,
+        on_delete=models.CASCADE,
+    )
+    medicamento = models.ForeignKey(
+        to=Medicamento,
+        on_delete=models.CASCADE,
+    )
+
